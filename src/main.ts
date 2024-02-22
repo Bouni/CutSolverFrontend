99d681941241fa98b8706e7881b3125e883662a6
Bouni <bouni@owee.de>
2024-02-22 09:05:41 +0100

    Add notifications and csv parser

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { BootstrapIconsPlugin } from "bootstrap-icons-vue";
+import VuePapaParse from 'vue-papa-parse'
+import Notifications from 'notiwind'
 import App from './App.vue'
 
 const app = createApp(App)
 
-app.use(createPinia()).use(BootstrapIconsPlugin)
+app.use(createPinia()).use(BootstrapIconsPlugin).use(VuePapaParse).use(Notifications)
+
+app.provide('$papa', app.config.globalProperties.$papa)
 
 app.mount('#app')
