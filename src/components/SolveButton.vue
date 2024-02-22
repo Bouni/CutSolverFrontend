a7c4f90c9ce35d12d76b0b16255500d5809fe1da
Bouni <bouni@owee.de>
2024-02-22 09:07:15 +0100

    Add solver metho to solve button

--- src/components/SolveButton.vue
+++ src/components/SolveButton.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <button class="btn btn-primary btn-outline btn-lg rounded-lg">Solve</button>
+    <button class="btn btn-primary btn-outline btn-lg rounded-lg" @click="store.solve()">Solve</button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useSolverStore } from '@/stores/solver'
+
+const store = useSolverStore()
+
+</script>
