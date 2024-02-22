d251330d61a77cbb60c99199c17cc449a90d7d29
Bouni <bouni@owee.de>
2024-02-22 16:34:46 +0100

    Basic CSV export

--- src/components/ExportCSV.vue
+++ src/components/ExportCSV.vue
@@ -1,12 +1,12 @@
 <template>
-  <button class="btn btn-outline" @click="downloadCSV()">
+  <button class="btn btn-outline" :class="store.solverResult.lengths.length == 0 ? 'btn-disabled' : ''"
+    @click="downloadCSV()">
     Export <b-icon-filetype-csv width="24" height="24" />
   </button>
 </template>
 
 <script setup lang="ts">
 import { inject } from "vue";
-import { notify } from "notiwind"
 import { useSolverStore } from '@/stores/solver'
 
 const store = useSolverStore()
@@ -14,13 +14,15 @@ const store = useSolverStore()
 const papa = inject("$papa");
 
 const downloadCSV = () => {
-  var csv = papa.unparse(store.getCSVData());
-  var csvData = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-  var csvURL = window.URL.createObjectURL(csvData);
-  var csvLink = document.createElement('a');
-  csvLink.href = csvURL;
-  csvLink.setAttribute('CSV', 'cutsolver.csv');
-  csvLink.click();
+  if (store.solverResult.lengths.length > 0) {
+    var csv = papa.unparse(store.getCSVData());
+    var csvData = new File([csv], "custolver.csv", { type: 'text/csv;charset=utf-8;' });
+    var csvURL = window.URL.createObjectURL(csvData);
+    var csvLink = document.createElement('a');
+    csvLink.href = csvURL;
+    csvLink.setAttribute('CSV', 'cutsolver.csv');
+    csvLink.click();
+  }
 }
 
 </script>
