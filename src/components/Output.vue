b343402960d6bb5ae7979c4bf2be3b163c787774
Bouni <bouni@owee.de>
2024-02-22 09:08:27 +0100

    Show solver method and time in subtitle

--- src/components/Output.vue
+++ src/components/Output.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="card bg-base-300">
-    <div class="card-body items-center text-center pt-2">
+    <div class="card-body items-center text-center pt-2" v-if="store.solverResult">
       <h2 class="card-title text-3xl font-normal mb-1">Output</h2>
-      <div>Solved using bruteforce. Calculation took 5.4ms.</div>
+      <div>Solved using {{ store.solverResult.solver_type }} in {{ store.solverResult.time_us }}us</div>
       <SolvedStock />
+      <Export />
     </div>
-    <Export />
+    <div class="card-body items-center text-center pt-2" v-else>
+      <h2 class="card-title text-3xl font-normal mb-1">Output</h2>
+      <p>Click solve to calculate</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import SolvedStock from "@/components/SolvedStock.vue"
 import Export from "@/components/Export.vue"
+import { useSolverStore } from '@/stores/solver'
+
+const store = useSolverStore()
 </script>
