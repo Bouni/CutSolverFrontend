f7da35230f0497a8d7b442b6c863b238a3301728
Bouni <bouni@owee.de>
2024-02-22 09:06:12 +0100

    Add notifications to App

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,19 @@
     <div class="mx-auto">
       <NavBar />
       <div class="flex flex-row w-full p-4">
-        <div class="w-full">
+        <div class="w-full mb-12">
           <Input />
         </div>
         <div class="w-64 flex justify-center items-center">
           <SolveButton />
         </div>
-        <div class="w-full">
+        <div class="w-full mb-12">
           <Output />
         </div>
       </div>
       <Footer />
     </div>
+    <Notifications />
   </main>
 </template>
 
@@ -26,4 +27,5 @@ import Input from "@/components/Input.vue"
 import SolveButton from "@/components/SolveButton.vue"
 import Output from "@/components/Output.vue"
 import Footer from "@/components/Footer.vue"
+import Notifications from "@/components/Notifications.vue"
 </script>
